448bef134d801b6fa9d5135d4b670e72d69f0543
ErosAnthonySerrano <erosanthonyserrano@gmail.com>
2026-04-11 00:30:24 +0800

    Test deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,9 @@ export default function Home() {
             center.
           </p>
         </div>
+        <span className="text-white">
+          {"this is a test to see if the env variables are working: " + process.env.NEXT_PUBLIC_SUPABASE_URL}
+        </span>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
